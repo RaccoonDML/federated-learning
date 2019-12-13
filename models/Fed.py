84d9049d2a6b862858113f314c0e1a0885ab6579
RaccoonDML <2684813584@qq.com>
2019-12-13 14:51:25 +0800

    Add weighted fed_avg based on 1/loss

--- models/Fed.py
+++ models/Fed.py
@@ -5,6 +5,7 @@
 import copy
 import torch
 from torch import nn
+import numpy as np
 
 
 def FedAvg(w):
@@ -17,7 +18,7 @@ def FedAvg(w):
 
 def FedAvg_weighted(w,losses):
     num=len(w)
-    losses=1/losses
+    losses=np.true_divide(1,losses)
     print(losses)
     w_avg = copy.deepcopy(w[0])
     for k in w_avg.keys():#对每一层的参数
@@ -26,3 +27,12 @@ def FedAvg_weighted(w,losses):
             w_avg[k] +=losses[i]*w[i][k]
         w_avg[k] = torch.div(w_avg[k], sum(losses))
     return w_avg
+
+def test(loss):
+    print(1 / loss)
+
+if __name__ == '__main__':
+    loss=np.array([[1,2],[4,5]])
+    print(1/loss)
+    test(loss)
+
